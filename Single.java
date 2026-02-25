87e5a8446c5d88d2369ef2ca479e50b67de22a66
AnjanaVP231 <anjanavp231@gmail.com>
2026-02-25 15:41:24 +0530

    Added Single Inheritance Program

---
+++ Single.java
@@ -0,0 +1,19 @@
+class Single {
+    public static void main(String[] args) {
+        class Animal {
+            void eat() {
+                System.out.println("Animal is eating");
+            }
+        }
+
+        class Dog extends Animal {
+            void bark() {
+                System.out.println("Dog is barking");
+            }
+        }
+
+        Dog obj = new Dog();
+        obj.eat();
+        obj.bark();
+    }
+}
