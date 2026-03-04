532a28dc935496bc3ced0c668ffa60580877088f
AnjanaVP231 <anjanavp231@gmail.com>
2026-03-04 15:19:15 +0530

    Updated

--- Nested.java
+++ Nested.java
@@ -1,19 +1,23 @@
-public class Nested {
-
-    static class OuterClass {
+public class Nested{
+    public static class OuterClass {
 
         class InnerClass {
             void display() {
                 System.out.println("Inside Inner Class");
             }
         }
+        void display_out() {
+                System.out.println("Inside Outer Class");
     }
-
+}
     public static void main(String[] args) {
 
         OuterClass outer = new OuterClass();
+        
         OuterClass.InnerClass inner = outer.new InnerClass();
-
+	
         inner.display();
+        outer.display_out();
     }
+
 }
